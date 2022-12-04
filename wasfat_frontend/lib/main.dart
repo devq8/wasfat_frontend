94dc390ad8c782b351739f0e63eed9685d0a37b3
Engiineeer <ntpzlwo@gmail.com>
2022-12-04 17:11:22 +0300

    list page

--- wasfat_frontend/lib/main.dart
+++ wasfat_frontend/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/wasfat_list_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      // home:
+      home: WasFatList(),
     );
   }
 }
--- wasfat_frontend/lib/pages/wasfat_list_page.dart
+++ wasfat_frontend/lib/pages/wasfat_list_page.dart
@@ -1,1 +1,56 @@
+import 'package:flutter/material.dart';
 
+class WasFatList extends StatelessWidget {
+  const WasFatList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.indigo,
+        appBar: AppBar(
+          title: Center(
+            child: Text("List"),
+          ),
+          backgroundColor: Colors.black,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                  maxCrossAxisExtent: 200,
+                  childAspectRatio: 3 / 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10),
+              itemCount: 7,
+              itemBuilder: (BuildContext ctx, index) {
+                return Container(
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.lightBlue,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Column(
+                    children: [
+                      Icon(Icons.image),
+                      Text(
+                        "name",
+                        style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        "description",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+        ),
+      ),
+    );
+  }
+}
